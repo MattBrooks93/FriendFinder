5a2e2fca3b9b166212617c039db987abf049c6f0
Matt Brooks <mattbrooks@matts-mbp.home>
2019-06-10 17:08:56 -0400

    Complete

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Matt",
+		photo: "https://live.staticflickr.com/2/2131827_dac4994d91_b.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Scottie",
+		photo: "https://c8.alamy.com/comp/BP65T1/poor-drunk-man-lying-near-trashcan-in-city-street-BP65T1.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "James",
+		photo: "https://pbs.twimg.com/profile_images/3095398398/8d66d39644dbbbb6edae9c0ad7c400c1_400x400.jpeg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Amanda",
+		photo: "https://thumbs.dreamstime.com/z/drunk-girl-smeared-makeup-posing-party-funny-91716832.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Karlee",
+		photo: "https://c8.alamy.com/comp/BPDNXT/drunk-girl-passed-out-by-bins-BPDNXT.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
